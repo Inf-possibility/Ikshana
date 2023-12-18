c34e62429bceea16ef7211833b5cccbda66cf629
K <karthik@hapikey.com>
2023-12-17 19:43:21 -0500

    React sample app back up

---
+++ src/App_react.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Ikshana</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Intelligence <code>delivered</code> with ai.
+        </p>
+        <a
+          className="App-link"
+          href="https://hapikey.retool.com/app/cosmop9newsaggregator_enhanced_uxab"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Ikshana ... news not noice
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
