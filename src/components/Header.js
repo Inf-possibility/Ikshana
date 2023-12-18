606cbf508f948b69fac09ed7c40bc8b5ce0a496d
K <karthik@hapikey.com>
2023-12-18 04:03:58 -0500

    taskbox animation and li

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,28 @@
 import logo from '../assets/logo.png';
 import '../components/header.css'
+import { useState } from "react";
 
 export const Header = () => {
-  const headername = 'Ikshana - News not notice '
-  const h2name = 'Follow Falcon'
-  return (
-    <div className="header-container">
-        <img className='logo' src={logo} alt="" />
+  const [isLogoClicked, setLogoClicked] = useState(false);
+  const headername = 'Ikshana Falcon'
+  const h2name = 'News not noise '
+
+    const handleLogoClick = () => {
+    setLogoClicked(true);
+    // Add a delay to remove the animation class after the animation is complete
+      setTimeout(() => {
+        setLogoClicked(false);
+      }, 4000);
+    };
+  
+    return (
+      <div className='header-container'>
+      <span className="logo-container" onClick={handleLogoClick}>
+        <img className={`logo ${isLogoClicked ? 'fly' : ''}`} src={logo} alt="" />
         <h1 className="header-text">{headername}</h1>
         <h1 className="h2-text">{h2name}</h1>
-    </div>
+      </span>
 
+    </div>
   )
 }
--- src/components/taskbox.js
+++ src/components/taskbox.js
@@ -1,47 +1,46 @@
-import { useState } from 'react';
-import '../components/taskbox.css'
+import { useState } from "react";
+import "../components/taskbox.css";
 
+  
 export const Taskbox = () => {
-    const [tasks, setTasks] = useState([
-        {id: 5271, name: "Record React Lectures", completed: true}
-      ]);
-    const boxname = 'Task List '
-  return (
-    <div className='taskBox'>
-      <h1>{boxname}</h1>
-      <ul>
-        { tasks.map((task) => (
-          <li key={task.id}>
-            <span>{task.id} - {task.name}</span>
-            <button className='delete'>Delete</button>
-          </li>
-        )) }
-      </ul>
-    </div>
-  )
-}
-
-
-function App() {
   const [tasks, setTasks] = useState([
-    {id: 5271, name: "Record React Lectures", completed: true}, 
-    {id: 7825, name: "Edit React Lectures", completed: false}, 
-    {id: 8391, name: "Watch Lectures", completed: false}
+    { id: 5271, name: "Times of India", completed: true },
+    { id: 7825, name: "CNN", completed: false },
+    { id: 2222, name: "AL Jazeera", completed: false },
+    { id: 3333, name: "Boston Globe", completed: false },
+    { id: 4444, name: "Washington Times", completed: false },
   ]);
+  const boxname = "Task List ";
+
+  function handleDelete(id){
+    console.log('deleteting the following' + id)
+    setTasks(tasks.filter(task => id !== task.id ))
+    }
+  
+
+
 
   return (
-    <div className="App">
-      <h1>Task List</h1>
+    <div className="taskBox">
+      <a>{boxname}</a>
       <ul>
-        { tasks.map((task) => (
+        <li>
+        <span>
+              1111 - You cannot delete refresh if you want 
+              <button onClick={() => handleDelete(3)} className="delete">Delete</button>
+            </span>
+        </li>
+        {tasks.map((task) => (
           <li key={task.id}>
-            <span>{task.id} - {task.name}</span>
-            <button className='delete'>Delete</button>
+            <span>
+              {task.id} - {task.name}
+              <button onClick={() => handleDelete(task.id)} className="delete">Delete</button>
+            </span>
           </li>
-        )) }
+        ))}
       </ul>
     </div>
   );
-}
+};
 
-export default App;
+
