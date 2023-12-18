5865f27f394bdbbbba13b867b47bd8d043efd5a0
K <karthik@hapikey.com>
2023-12-18 04:25:29 -0500

    task show completed

--- src/components/taskbox.js
+++ src/components/taskbox.js
@@ -1,46 +1,49 @@
 import { useState } from "react";
 import "../components/taskbox.css";
 
-  
 export const Taskbox = () => {
   const [tasks, setTasks] = useState([
     { id: 5271, name: "Times of India", completed: true },
     { id: 7825, name: "CNN", completed: false },
     { id: 2222, name: "AL Jazeera", completed: false },
-    { id: 3333, name: "Boston Globe", completed: false },
+    { id: 3333, name: "Boston Globe", completed: true },
     { id: 4444, name: "Washington Times", completed: false },
   ]);
+
+  const [show, setShow] = useState(false);
   const boxname = "Task List ";
 
-  function handleDelete(id){
-    console.log('deleteting the following' + id)
-    setTasks(tasks.filter(task => id !== task.id ))
-    }
-  
-
-
+  function handleDelete(id) {
+    console.log("deleteting the following" + id);
+    setTasks(tasks.filter((task) => id !== task.id));
+  }
 
   return (
     <div className="taskBox">
-      <a>{boxname}</a>
+      <a>
+        <button className="trigger" onClick={() => setShow(!show)}>
+          Toggle {boxname}
+        </button>
+      </a>
       <ul>
-        <li>
-        <span>
-              1111 - You cannot delete refresh if you want 
-              <button onClick={() => handleDelete(3)} className="delete">Delete</button>
-            </span>
-        </li>
-        {tasks.map((task) => (
-          <li key={task.id}>
-            <span>
-              {task.id} - {task.name}
-              <button onClick={() => handleDelete(task.id)} className="delete">Delete</button>
-            </span>
-          </li>
-        ))}
+        {show &&
+          tasks.map((task) => (
+            <li
+              key={task.id}
+              className={task.completed ? "completed" : "incomplete"}
+            >
+              <span>
+                {task.id} - {task.name}
+                <button
+                  onClick={() => handleDelete(task.id)}
+                  className="delete"
+                >
+                  Delete
+                </button>
+              </span>
+            </li>
+          ))}
       </ul>
     </div>
   );
 };
-
-
